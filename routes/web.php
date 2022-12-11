7a390d863164941664549e3b345e9870b014132a
MdAnisuzzaman <anism7898@gmail.com>
2022-12-11 11:47:57 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,'index'])->name('home');
-Route::get('/product',[HomeController::class,'product'])->name('product');
+Route::get('/productshow',[HomeController::class,'product'])->name('product.show');
 
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])->group(function ()
